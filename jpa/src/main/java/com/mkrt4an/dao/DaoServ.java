f68c33516c52aae79d77efcd22e18c64291fe017
mkrt4an <mcrtchan@mail.ru>
2016-10-02 10:57:25 +0300

    JPS print all entity in database

--- jpa/src/main/java/com/mkrt4an/dao/DaoServ.java
+++ jpa/src/main/java/com/mkrt4an/dao/DaoServ.java
@@ -5,25 +5,39 @@ import com.mkrt4an.entity.UserEntity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 /**
  * Created by 123 on 28.09.2016.
  */
 public class DaoServ {
-    public static void main(String[] args) {
+    public List<UserEntity> doGetMore() {
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("NewPersistenceUnit");
+//        EntityManager em = emf.createEntityManager();
+//        UserEntity entity = em.find(UserEntity.class, 2);
+//        return entity.toString();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("NewPersistenceUnit");
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<UserEntity> typedQuery;
+        typedQuery = em.createQuery("SELECT u from UserEntity u ", UserEntity.class);
+        List<UserEntity> resultList = typedQuery.getResultList();
+        return resultList;
+    }
+
+    public static void main(String[] args)  {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("NewPersistenceUnit");
         EntityManager em = emf.createEntityManager();
         UserEntity entity = em.find(UserEntity.class, 2);
 
         System.out.println(entity);
 
+        TypedQuery<UserEntity> typedQuery;
+        typedQuery = em.createQuery("SELECT u from UserEntity u ", UserEntity.class);
+        List<UserEntity> resultList = typedQuery.getResultList();
 
-//        TypedQuery<UserEntity> typedQuery;
-//        typedQuery = em.createQuery("SELECT u from UserEntity u", UserEntity.class);
-//        List<UserEntity> resultList = typedQuery.getResultList();
 
-//
-//        System.out.println(resultList);
+        System.out.println(resultList);
     }
 
 }
--- web/src/main/webapp/index.jsp
+++ web/src/main/webapp/index.jsp
@@ -1,8 +1,30 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+
+<%@ page contentType="text/html; charset=windows-1251" %>
+<%@ page import="com.mkrt4an.dao.DaoServ, com.mkrt4an.entity.UserEntity" %>
+<%@ page import="javax.persistence.EntityManager" %>
+<%--<%@ page import="static com.mkrt4an.dao.DaoServ.doGet" %>--%>
+<%@ page import="javax.persistence.EntityManagerFactory" %>
+<%@ page import="javax.persistence.Persistence" %>
+<%@ page import="javax.persistence.TypedQuery" %>
+
+<%@ page import="java.util.List" %>
+
 <html>
     <body>
         <h2>
-            <c:out value="${UserEntity.name}"/>
-                <%--<%out.println("Your IP address is " + request.getRemoteAddr());%>--%>
+            Добро пожаловать! Сегодня <%= new DaoServ().doGetMore() %>
         </h2>
     </body>
 </html>
+
+<%--<%!--%>
+    <%--List<UserEntity> getFormattedDate () <%= new DaoServ().doGetMore() %>{--%>
+        <%--EntityManagerFactory emf = Persistence.createEntityManagerFactory("NewPersistenceUnit");--%>
+        <%--EntityManager em = emf.createEntityManager();--%>
+        <%--TypedQuery<UserEntity> typedQuery;--%>
+        <%--typedQuery = em.createQuery("SELECT u from UserEntity u", UserEntity.class);--%>
+        <%--List<UserEntity> resultList = typedQuery.getResultList();--%>
+        <%--return resultList;--%>
+    <%--}--%>
+<%--%>--%>
